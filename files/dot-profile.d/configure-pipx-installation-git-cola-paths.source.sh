fe0545594f72bc27aa46d633041eff46e3c3ea3e
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2024-10-04 17:59:36 +0800

    fix(git-cola): Drop is_dir_in_paths .profile.d function definition
    Implemented upstream in v2.1.0.
    
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- files/dot-profile.d/configure-pipx-installation-git-cola-paths.source.sh
+++ files/dot-profile.d/configure-pipx-installation-git-cola-paths.source.sh
@@ -4,41 +4,6 @@
 # Copyright 2024 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-## Check if the dir is already in paths
-is_dir_in_paths(){
-    if test "${#}" -ne 2; then
-        printf \
-            'is_dir_in_paths: Error: This function should have 2 arguments, only %s is specified.\n' \
-            "${#}" \
-            1>&2
-        return 2
-    fi
-
-    dir="${1}"; shift
-    paths="${1}"; shift
-
-    # Backup IFS
-    IFS_ORIGINAL="${IFS}"
-    IFS=":"
-
-    path_found=false
-    for path in ${paths}; do
-        if [ "${dir}" = "${path}" ]; then
-            path_found=true
-            break
-        fi
-    done
-
-    # Restore IFS
-    IFS="${IFS_ORIGINAL}"
-
-    if test "${path_found}" = false; then
-        return 1
-    else
-        return 0
-    fi
-}
-
 git_cola_pipx_data_dir="${HOME}/.local/pipx/venvs/git-cola/share"
 if ! test -e "${git_cola_pipx_data_dir}"; then
     return
