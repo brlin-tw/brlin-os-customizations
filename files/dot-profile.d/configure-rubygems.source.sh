c3e7323c488a2f0c7eb90c952d782f4c86ef752e
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2024-10-05 18:07:10 +0800

    feat: Configure RubyGems packages installations in the local user session
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

---
+++ files/dot-profile.d/configure-rubygems.source.sh
@@ -0,0 +1,20 @@
+# shellcheck shell=sh
+# Configure RubyGems packages installations in the local user
+#
+# Copyright 2024 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# Install Ruby Gems to ~/.gems
+GEM_HOME="${HOME}/.gems"
+gem_executables_dir="${GEM_HOME}/bin"
+
+if test -z "${PATH}"; then
+    PATH="${gem_executables_dir}"
+    export PATH
+else
+    if ! is_dir_in_paths "${gem_executables_dir}" "${PATH}"; then
+        PATH="${gem_executables_dir}:${PATH}"
+    fi
+fi
+
+export GEM_HOME
