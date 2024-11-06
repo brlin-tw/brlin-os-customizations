923c43b09d5b39300852fb5673c3ec7fdd1b9257
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2024-11-06 16:40:12 +0800

    fix(profile.d/android-dev): Fix JAVA_HOME not set error
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- files/dot-profile.d/configure-android-build-environment.source.sh
+++ files/dot-profile.d/configure-android-build-environment.source.sh
@@ -10,3 +10,15 @@ if command -v ccache >/dev/null; then
     USE_CCACHE=1
     export USE_CCACHE
 fi
+
+# Resolve the following error message during build:
+#
+#    JAVA_HOME is not set.
+#    Please set JAVA_HOME so that switching between Android Studio and the terminal does not trigger a full rebuild."
+#
+studio_prefix="${HOME}/應用軟體/android-studio"
+studio_java_dir="${studio_prefix}/jbr"
+if test -e "${studio_java_dir}"; then
+    JAVA_HOME="${studio_java_dir}"
+    export JAVA_HOME
+fi
