3efc91f3c126b088022811ac8eda03ff57067d9b
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2024-11-08 01:45:59 +0800

    feat(profile): Support configuring the Ubuntu developer identity
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

---
+++ files/dot-profile.d/configure-ubuntu-developer-identity.source.sh
@@ -0,0 +1,14 @@
+# shellcheck shell=sh
+# Configure the Ubuntu developer identity
+#
+# References:
+#
+# * Introducing yourself · Environment variables · ubuntu-packaging-guide/docs/how-to/ubuntu-dev-tools.rst at 1.0 · canonical/ubuntu-packaging-guide
+#   https://github.com/canonical/ubuntu-packaging-guide/blob/1.0/docs/how-to/ubuntu-dev-tools.rst#introducing-yourself
+#
+# Copyright 2024 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+UBUEMAIL="林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>"
+
+export UBUEMAIL
